7e45ad0672a668ab79820a89b04e05505115ffa9
Kelvin Akaba <akabakelvin@gmail.com>
2024-01-27 13:56:58 +0000

    input width

--- components/Form.tsx
+++ components/Form.tsx
@@ -128,7 +128,7 @@ const Form = () => {
                         </div>
                         <div className="mb-5">
                             <label htmlFor="dob" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Date of Birth</label>
-                            <div className="relative w-full">
+                            <div className="relative max-w-xl">
                                 <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                                     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
