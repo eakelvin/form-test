180f65e96fb091df17e7fa06573f70581b4a028e
Kelvin Akaba <akabakelvin@gmail.com>
2024-01-27 13:39:11 +0000

    validation && error

--- components/Form.tsx
+++ components/Form.tsx
@@ -23,10 +23,12 @@ const steps = [
 ]
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors, isSubmitSuccessful }, reset } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors, isSubmitSuccessful, isValid }, reset } = useForm<FormData>();
     const [formData, setFormData] = useState<FormData | null>(null);
     const [currentStep, setCurrentStep] = useState(0)
     const [showInput, setShowInput] = useState<boolean>(false)
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
 
     const onSubmit = async (data: FormData) => {
         try {
@@ -44,8 +46,12 @@ const Form = () => {
     }
 
     const next = () => {
-        if (currentStep < steps.length - 1) {
-            setCurrentStep(step => step + 1)
+        if (isValid) {
+            if (currentStep < steps.length - 1) {
+                setCurrentStep(step => step + 1)
+            }          
+        } else {
+            setErrorMessage('Fill in the details to continue') 
         }
     }
 
@@ -57,7 +63,7 @@ const Form = () => {
 
 
     return (
-        <div>
+        <div className=''>
             <nav aria-label='Progress'>
                 <ol role='list' className='space-y-4 md:flex md:space-x-8 md:space-y-0'>
                     {steps.map((step, index) => (
@@ -104,9 +110,9 @@ const Form = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Enter your first name"
                                 required
-                                {...register(`personalInfo.${0}.firstName`)}
-                            // {errors.firstName && <p> {errors.firstName.message} </p>}
+                                {...register(`personalInfo.${0}.firstName`, { required: true })}  
                             />
+                             {errors.personalInfo?.[0]?.firstName && ( <p>FirstName is Required</p> )}
                         </div>
                         <div className='mb-5'>
                             <label htmlFor="lastName" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Last name</label>
@@ -116,12 +122,13 @@ const Form = () => {
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 placeholder="Enter your last name"
                                 required
-                                {...register(`personalInfo.${0}.lastName`)}
+                                {...register(`personalInfo.${0}.lastName`, { required: true })}
                             />
+                            {errors.personalInfo?.[0]?.lastName && ( <p>LastName is Required</p> )}
                         </div>
                         <div className="mb-5">
                             <label htmlFor="dob" className="block mb-2 text-md font-medium text-gray-900 dark:text-white">Date of Birth</label>
-                            <div className="relative max-w-sm">
+                            <div className="relative">
                                 <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
                                     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                                         <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
@@ -131,18 +138,26 @@ const Form = () => {
                                     type="date"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Select date"
-                                    {...register(`personalInfo.${0}.dob`)}
+                                    {...register(`personalInfo.${0}.dob`, { required: true })}
                                 />
+                                {errors.personalInfo?.[0]?.dob && ( <p>Date of Birth is Required</p> )}
                             </div>
                         </div>
+                        {errorMessage && (
+                            <div className="text-red-500 mt-2">
+                                {errorMessage}
+                            </div>
+                        )}
                         <div className='flex justify-center'>
                             <button
+                                type='button'
                                 onClick={next}
                                 disabled={currentStep === steps.length - 1}
-                                type="button"
-                                className="mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
                             >
-                                Next
+                                <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='h-6 w-6'>
+                                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
+                                </svg>
                             </button>
                         </div>
                     </div>
@@ -212,7 +227,7 @@ const Form = () => {
                         <button
                             type='button'
                             onClick={prev}
-                            disabled={!currentStep}
+                            // disabled={!currentStep}
                             className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
                         >
                             <svg
@@ -241,10 +256,10 @@ const Form = () => {
                 )}
 
                 {currentStep === 2 && formData && (
-                    <div className='mt-8 text-white'>
+                    <div className='mt-8'>
                         <h1 className='font-bold text-center text-2xl'>Complete</h1>
                         <p className='text-xl-leading-6 text-center'>Thank you for your submission</p>
-                        <p className='mt-1'>A Sample JSON</p>
+                        <p className='mt-1'>Sample JSON of your Information</p>
                         <pre>{JSON.stringify(formData, null, 2)}</pre>
                     </div>
                 )}
@@ -296,7 +311,6 @@ const Form = () => {
             </button>
             </div>
             </div> */}
-
         </div>
     )
 }
