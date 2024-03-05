b8cd2887ecbc8fc6145939b41bf109a1e1856ff3
Kelvin Akaba <akabakelvin@gmail.com>
2024-03-05 22:32:18 +0000

    test input date

--- components/Form.tsx
+++ components/Form.tsx
@@ -133,7 +133,7 @@ const Form = () => {
                             <input
                                 type="date"
                                 id="dob"
-                                className="bg-gray-50 border border-gray-300 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                className="!inline-flex bg-gray-50 border border-gray-300 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full p-2.5"
                                 required
                                 {...register(`personalInfo.${0}.dob`, { required: true })}
                             />
@@ -290,53 +290,6 @@ const Form = () => {
                     </div>
                 )}
             </form>
-
-            {/* <div className='mt-8 pt-5'>
-            <div className='flex justify-between'>
-            <button
-                type='button'
-                onClick={prev}
-                disabled={currentStep === 0}
-                className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
-            >
-                <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='h-6 w-6'
-                >
-                <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M15.75 19.5L8.25 12l7.5-7.5'
-                />
-                </svg>
-            </button>
-            <button
-                type='button'
-                onClick={next}
-                disabled={currentStep === steps.length - 1}
-                className='rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50'
-            >
-                <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth='1.5'
-                stroke='currentColor'
-                className='h-6 w-6'
-                >
-                <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    d='M8.25 4.5l7.5 7.5-7.5 7.5'
-                />
-                </svg>
-            </button>
-            </div>
-            </div> */}
         </div>
     )
 }
